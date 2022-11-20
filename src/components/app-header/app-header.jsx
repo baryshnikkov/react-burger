0f379f00af1f9af62e9e5f85fdd061bf5d50df5c
Maxim Baryshnikov <baryshnikkov@gmail.com>
2022-11-20 23:09:49 +0300

    feat: реализован компонент AppHeader

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import styles from './app.module.css';
+
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components'
+
+function AppHeader() {
+  return (
+    <header className={[styles.header, 'p-4'].join(' ')}>
+      <nav className={styles.nav}>
+
+        <div className={styles.menu}>
+          <div className={[styles.link, 'pt-4', 'pr-5', 'pb-4', 'pl-5'].join(' ')}>
+            <BurgerIcon type="primary" />
+            <p className="text text_type_main-default">
+              Конструктор
+            </p>
+          </div>
+          <div className={[styles.link, 'pt-4', 'pr-5', 'pb-4', 'pl-5'].join(' ')}>
+            <ListIcon type="secondary" />
+            <p className="text text_type_main-default text_color_inactive">
+              Лента заказов
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.link}>
+          <Logo />
+        </div>
+
+        <div className={[styles.link, 'pt-4', 'pr-5', 'pb-4', 'pl-5'].join(' ')}>
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default text_color_inactive">
+            Личный кабинет
+          </p>
+        </div>
+
+      </nav>
+    </header>
+  );
+}
+
+export default AppHeader;
---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './app.module.css';
+
+import AppHeader from '../app-header/app-header';
+
+function App() {
+  return (
+    <AppHeader />
+  );
+}
+
+export default App;
