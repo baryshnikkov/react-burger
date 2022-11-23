6ee9fd2a84dd35b43c281098fe22b3bc19b609bb
Maxim Baryshnikov <baryshnikkov@gmail.com>
2022-11-23 23:52:07 +0300

    feat: реализован компонент BurgerConstructor

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,20 +1,67 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './burger-constructor.module.css';
 
-import { ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import products from '../utils/data';
+import { ConstructorElement, Button, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function BurgerConstructor(props) {
 
-  const [rolls, setRolls] = React.useState({ name: 'James', surname: 'Wilson' });
-  const [ingredients, setIngredients] = React.useState([]);
+  const totalCalculation = () => {
+    let count = 0;
+    props.products.other.forEach(el => {
+      count += el['price'];
+    });
+    return count;
+  }
 
   return (
-    <div className={[styles.container, 'pt-25'].join(' ')}>
+    <div className={[styles.container, 'mt-25'].join(' ')}>
+      <ConstructorElement
+        type="top"
+        isLocked={true}
+        text={props.products.bun['name'] + ' (верх)'}
+        price={200}
+        thumbnail={props.products.bun['image']}
+      />
+
+      <div className={styles.ingredients}>
+        {props.products.other.map(el => (
+          <div style={{ display: 'flex', gap: '10px', alignItems: 'center' }} key={el['_id']}>
+            <DragIcon type="primary" />
+            <ConstructorElement
+              text="Краторная булка N-200i (верх)"
+              price={el['price']}
+              thumbnail={el['image']}
+            />
+          </div>
+        ))}
+      </div>
+
+      <ConstructorElement
+        type="bottom"
+        isLocked={true}
+        text={props.products.bun['name'] + ' (низ)'}
+        price={200}
+        thumbnail={props.products.bun['image']}
+      />
+
+      <div className={styles.oder}>
+        <div className={styles.price}>
+          <p className="text text_type_digits-medium">{totalCalculation()}</p>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button htmlType="button" type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
 
     </div>
   );
+}
+
+BurgerConstructor.propTypes = {
+  products: PropTypes.object,
 }
 
 export default BurgerConstructor;
