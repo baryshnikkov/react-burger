bd1cfd6a0915e05a33c153eba77c092f32c16957
Maxim Baryshnikov <baryshnikkov@gmail.com>
2022-11-30 18:12:00 +0500

    fix: исправлены ошибки при открытии модального окна IngredientDetails

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,28 +4,22 @@ import PropTypes from 'prop-types';
 import styles from './card.module.css';
 
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
-import IngredientDetails from '../ingredient-details/ingredient-details';
 
 function Card(props) {
-  const [details, setDetails] = React.useState({});
-
   const getDetails = () => {
-    setDetails(props);
+    props.getDetails(props);
   };
 
   return (
-    <>
-      <div className={styles.card} onClickCapture={getDetails}>
-        {Boolean(props.__v) && <Counter count={props.__v} size="default" extraClass="m-1" />}
-        <img className={styles.img} src={props.image} alt={props.name} />
-        <div className={[styles.price, 'mt-2', 'mb-2'].join(' ')}>
-          <p className={['text', 'text_type_digits-default'].join(' ')}>{props.price}</p>
-          <CurrencyIcon type="primary" />
-        </div>
-        <p className={[styles.description, 'text', 'text_type_main-default'].join(' ')}>{props.name}</p>
+    <div className={styles.card} onClickCapture={getDetails}>
+      {Boolean(props.__v) && <Counter count={props.__v} size="default" extraClass="m-1" />}
+      <img className={styles.img} src={props.image} alt={props.name} />
+      <div className={[styles.price, 'mt-2', 'mb-2'].join(' ')}>
+        <p className={['text', 'text_type_digits-default'].join(' ')}>{props.price}</p>
+        <CurrencyIcon type="primary" />
       </div>
-      <IngredientDetails details={details} />
-    </>
+      <p className={[styles.description, 'text', 'text_type_main-default'].join(' ')}>{props.name}</p>
+    </div>
   );
 }
 
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -4,20 +4,29 @@ import PropTypes from 'prop-types';
 import styles from './cards.module.css';
 
 import Card from '../card/card';
+import IngredientDetails from '../ingredient-details/ingredient-details';
 
 function Cards(props) {
   const [ingredients, setIngredients] = React.useState([]);
+  const [details, setDetails] = React.useState({});
+
+  const getDetails = (value) => {
+    setDetails(value);
+  };
 
   useEffect(() => {
     setIngredients(props.content);
   }, [props.content]);
 
   return (
-    <div className={styles.cards}>
-      {ingredients.map(el => (
-        <Card {...el} key={el['_id']} />
-      ))}
-    </div>
+    <>
+      <div className={styles.cards}>
+        {ingredients.map(el => (
+          <Card {...el} key={el['_id']} getDetails={getDetails} />
+        ))}
+      </div>
+      <IngredientDetails details={details} />
+    </>
   );
 }
 
