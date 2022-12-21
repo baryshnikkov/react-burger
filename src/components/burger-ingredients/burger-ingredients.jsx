2a770805f1a693ac296bf189bf10ea23aa1d7d79
Maxim Baryshnikov <baryshnikkov@gmail.com>
2022-12-21 20:22:39 +0300

    fix: настройка  навигации по ингредиентам

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import styles from './burger-ingredients.module.css';
@@ -15,6 +15,11 @@ function BurgerIngredients() {
   const { ingredients } = useSelector(store => store.ingredients);
   const { ingredientDataModalIsOpened } = useSelector(store => store.dataAboutIngredient);
   const dispatch = useDispatch();
+
+  const container = useRef();
+  const buns = useRef();
+  const sauces = useRef();
+  const fillings = useRef();
 
   const filteredIngredientsByType = useMemo(() => {
     const buns = [];
@@ -48,9 +53,25 @@ function BurgerIngredients() {
     });
   };
 
+  const setTab = (state, element) => {
+    element.current.scrollIntoView();
+  };
+
   useEffect(() => {
     dispatch(getIngredients());
   }, []);
+
+  const handleScroll = () => {
+    if (container.current.getBoundingClientRect().top + 1 > buns.current.getBoundingClientRect().top) {
+      setCurrent('buns');
+    }
+    if (container.current.getBoundingClientRect().top + 1 > sauces.current.getBoundingClientRect().top) {
+      setCurrent('sauces');
+    }
+    if (container.current.getBoundingClientRect().top + 1 > fillings.current.getBoundingClientRect().top) {
+      setCurrent('fillings');
+    }
+  };
 
   return (
     <>
@@ -60,29 +81,29 @@ function BurgerIngredients() {
         </h1>
 
         <div className={styles.tab}>
-          <Tab value="buns" active={current === 'buns'} onClick={setCurrent}>
+          <Tab value="buns" active={current === 'buns'} onClick={(e) => {setTab(e, buns)}}>
             Булки
           </Tab>
-          <Tab value="sauces" active={current === 'sauces'} onClick={setCurrent}>
+          <Tab value="sauces" active={current === 'sauces'} onClick={(e) => {setTab(e, sauces)}}>
             Соусы
           </Tab>
-          <Tab value="fillings" active={current === 'fillings'} onClick={setCurrent}>
+          <Tab value="fillings" active={current === 'fillings'} onClick={(e) => {setTab(e, fillings)}}>
             Начинки
           </Tab>
         </div>
 
-        <div className={styles.ingredients}>
-          <h2 className={['text', 'text_type_main-medium', 'mt-10', 'mb-6'].join(' ')}>
+        <div className={styles.ingredients} ref={container} onScroll={handleScroll}>
+          <h2 className={['text', 'text_type_main-medium', 'mt-10', 'mb-6'].join(' ')} ref={buns}>
             Булки
           </h2>
           <Cards content={filteredIngredientsByType.buns} />
 
-          <h2 className={['text', 'text_type_main-medium', 'mt-10', 'mb-6'].join(' ')}>
+          <h2 className={['text', 'text_type_main-medium', 'mt-10', 'mb-6'].join(' ')} ref={sauces}>
             Соусы
           </h2>
           <Cards content={filteredIngredientsByType.sauces} />
 
-          <h2 className={['text', 'text_type_main-medium', 'mt-10', 'mb-6'].join(' ')}>
+          <h2 className={['text', 'text_type_main-medium', 'mt-10', 'mb-6'].join(' ')} ref={fillings}>
             Начинки
           </h2>
           <Cards content={filteredIngredientsByType.fillings} />
