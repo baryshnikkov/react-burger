7019a6b1d7b5c288438ab80a939341e56ba178f4
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-10-17 22:45:38 +0300

    настраиваем NotFoundPage

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,12 +2,14 @@ import { RuleSetRule } from "webpack";
 import { BuildOptions } from "./types/config";
 import { buildTsLoader } from "./loaders/buildTsLoader";
 import { buildCssLoaders } from "./loaders/buildCssLoader";
+import { buildSvgLoader } from "./loaders/buildSvgLoader";
 
 export function buildLoaders(options: BuildOptions): RuleSetRule[] {
 	const { isDev } = options;
 
 	const tsLoader = buildTsLoader();
 	const cssLoader = buildCssLoaders(isDev);
+	const svgLoader = buildSvgLoader();
 
-	return [tsLoader, cssLoader];
+	return [tsLoader, cssLoader, svgLoader];
 }
