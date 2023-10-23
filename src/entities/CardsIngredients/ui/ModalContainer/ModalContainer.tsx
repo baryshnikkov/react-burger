289dc2414984069ae7d99bc9ed6586a78f7627a2
Maxim Baryshnikov <baryshnikkov@gmail.com>
2023-10-23 16:23:57 +0600

    настаиваем отображение контента Modal

--- src/entities/CardsIngredients/ui/ModalContainer/ModalContainer.tsx
+++ src/entities/CardsIngredients/ui/ModalContainer/ModalContainer.tsx
@@ -14,9 +14,40 @@ export interface ModalContainerProps {
 }
 
 const ModalContainer = memo((props: ModalContainerProps) => {
-	const { className, proteins, fat, carbohydrates, calories } = props;
+	const { className, proteins, fat, carbohydrates, calories, image, name } =
+		props;
 
-	return <div className={cn(cls.ModalContainer, {}, [className])}>modal</div>;
+	const displayEnergyValue = (text: string, value: number) => {
+		return (
+			<div className={cls.bju}>
+				<p className="text text_type_main-default text_color_inactive">
+					{text}
+				</p>
+				<p className="text text_type_main-default text_color_inactive">
+					{value}
+				</p>
+			</div>
+		);
+	};
+
+	return (
+		<div className={cn(cls.ModalContainer, {}, [className])}>
+			<p className={`${cls.title} text text_type_main-large`}>
+				Детали ингредиента
+			</p>
+
+			<img className={cls.image} src={image} alt={name} />
+
+			<p className={`${cls.name} text text_type_main-medium`}>{name}</p>
+
+			<div className={cls.compound}>
+				{displayEnergyValue("Калории,ккал", calories)}
+				{displayEnergyValue("Белки, г", proteins)}
+				{displayEnergyValue("Жиры, г", fat)}
+				{displayEnergyValue("Углеводы, г", carbohydrates)}
+			</div>
+		</div>
+	);
 });
 
 export default ModalContainer;
